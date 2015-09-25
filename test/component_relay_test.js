1c417b45a2f2114f4f3131dfc9b5797cfd4348db
nomilous <nomilous@gmail.com>
2015-09-25 22:36:11 +0200

    calls server.relayEvented() if no connection is possible/available

--- test/component_relay_test.js
+++ test/component_relay_test.js
@@ -10,7 +10,6 @@ objective('ComponentRelay', function() {
 
   context('calls server.relayConnected() if datalayer connectable', function() {
 
-
     before(function(done, Promise, happner, GroupConfig, PersonConfig, ComponentRelay) {
       this.timeout(2000);
       Promise.all([
@@ -24,7 +23,7 @@ objective('ComponentRelay', function() {
         mock('person1', person1);
         mock('person2', person2);
         mock('person3', person3);
-        mock('relay', group1._mesh.elements.relay.module.instance);
+        mock('relay1', group1._mesh.elements.relay.module.instance);
       })
       .then(done)./*catch(done)*/catch(function(e) {
         if (! e instanceof Error) {
@@ -35,11 +34,12 @@ objective('ComponentRelay', function() {
       });
     });
 
-    after(function(done, Promise, group1, person1, person2) {
+    after(function(done, Promise, group1, person1, person2, person3) {
       Promise.all([
         group1.stop(),
         person1.stop(),
         person2.stop(),
+        person3.stop(),
       ]).then(done).catch(done);
     });
 
@@ -47,7 +47,7 @@ objective('ComponentRelay', function() {
       
     it('makes new connection',
 
-      function(done, /* ComponentRelay, */ Server, relay, group1, person1, expect) {
+      function(done, /* ComponentRelay, */ Server, relay1, group1, person1, expect) {
 
         // var relay;
         // mock(ComponentRelay.prototype).spy(
@@ -68,14 +68,22 @@ objective('ComponentRelay', function() {
 
         // expect 1 call the relayConnected()
 
-        Server.does(function relayConnected(a, b, callback) {callback()});
+        Server.does(function relayConnected(happn, connection, relaySpec, callback) {callback()});
+
+        // expect 1 call to addConnection
+
+        relay1.does(
+          function addConnection() {
+            mock.original.apply(this, arguments);
+          }
+        );
 
         // person initiates relay of his/her private thing
 
         person1.exchange.group1.relay.create(relaySpec1)
 
         .then(function() {
-          expect(Object.keys(   relay.connections   )).to.eql(['127.0.0.1:20001']);
+          expect(Object.keys(   relay1.connections   )).to.eql(['127.0.0.1:20001']);
         })
 
         .then(done).catch(done);
@@ -84,7 +92,7 @@ objective('ComponentRelay', function() {
 
     it('makes two relays with one connection on concurrent call to relay',
 
-      function(done, Promise, Server, relay, group1, person2, expect) {
+      function(done, Promise, Server, relay1, group1, person2) {
 
         var $happn1 = person2._mesh.elements.thing1.component.instance;
         var $happn2 = person2._mesh.elements.thing2.component.instance;
@@ -113,7 +121,7 @@ objective('ComponentRelay', function() {
 
         // expect 1 call to addConnection
 
-        relay.does(
+        relay1.does(
           function addConnection() {
             mock.original.apply(this, arguments);
           }
@@ -128,9 +136,9 @@ objective('ComponentRelay', function() {
       }
     );
 
-    it.only('uses existing connection',
+    it('uses existing connection',
 
-      function(done, Server, relay, group1, person3, expect) {
+      function(done, Server, relay1, group1, person3) {
 
         var $happn1 = person3._mesh.elements.thing1.component.instance;
         var $happn2 = person3._mesh.elements.thing2.component.instance;
@@ -168,7 +176,7 @@ objective('ComponentRelay', function() {
 
           // expct no calls to addConnection
 
-          relay.spy(
+          relay1.spy(
             function addConnection() {
               throw new Error('called addConnection');
             }
@@ -178,21 +186,103 @@ objective('ComponentRelay', function() {
 
         })
 
-        .then(function(r) {
-          console.log(r);
-          done();
-        })
-
-        .catch(function(e) {
-          console.log(e);
-          done(e);
-        });
+        .then(done).catch(done);
       }
     );
 
   });
 
 
-  context('calls server.relayEvented() if no connection is possible/available');
+  context('calls server.relayEvented() if no connection is possible/available', function() {
+
+    before(function(done, Promise, happner, GroupConfig, PersonConfig, ComponentRelay) {
+      this.timeout(2000);
+      Promise.all([
+        happner.create(GroupConfig(2)),
+        happner.create(PersonConfig(4, 2)),
+        happner.create(PersonConfig(5, 2)),
+        // happner.create(PersonConfig(6, 2)),
+      ])
+      .spread(function(group2, person4, person5, person6) {
+        mock('group2', group2);
+        mock('person4', person4);
+        mock('person5', person5);
+        mock('person6', person6);
+        mock('relay2', group2._mesh.elements.relay.module.instance);
+      })
+      .then(done)./*catch(done)*/catch(function(e) {
+        if (! e instanceof Error) {
+          console.log('ERROR1', e); // <----------------------------------- TODO: getting errors that are not instanceof Error out of happn/happner, eradicate this behaviour.
+          return done(new Error());
+        }
+        done(e);
+      });
+    });
+
+    after(function(done, Promise, group2, person4, person5, person6) {
+      Promise.all([
+        group2.stop(),
+        person4.stop(),
+        person5.stop(),
+        // person6.stop(),
+      ]).then(done).catch(done);
+    });
+
+    it('first attempts connection if target provided', 
+
+      function(done, Server, relay2, group2, person4) {
+
+
+        var $happn = person4._mesh.elements.thing1.component.instance;
+
+        var unaccessable = $happn.info.datalayer.address;
+        unaccessable.port = 44444; // not listening
+
+        var relaySpec = {
+          target: unaccessable,
+          component: {
+            name: 'person4_thing1',
+            description: $happn.description,
+          }
+        };
+
+        Server.does(
+          function relayEvented(happn, relaySpec, callback) {callback()}
+        );
+
+        person4.exchange.group2.relay.create(relaySpec)
+
+        .then(done).catch(done);
+
+      }
+    );
+
+    it('attempts no connection if no target provided',
+
+      function(done, Server, relay2, group2, person5) {
+
+        var $happn = person5._mesh.elements.thing1.component.instance;
+
+        var relaySpec = {
+          // target: $happn.info.datalayer.address,
+          component: {
+            name: 'person5_thing1',
+            description: $happn.description,
+          }
+        };
+
+        Server.does(
+          function relayEvented(happn, relaySpec, callback) {callback()}
+        );
+
+        person5.exchange.group2.relay.create(relaySpec)
+
+        .then(done).catch(done);
+
+
+      }
+    );
+
+  });
 
 });
