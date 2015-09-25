7cede76d370ab1f0237b23e679e5841590efb0bb
nomilous <nomilous@gmail.com>
2015-09-25 10:54:38 +0200

    creates only one connection for multiple relays to the same remote

--- test/component_relay_test.js
+++ test/component_relay_test.js
@@ -3,6 +3,8 @@ objective('ComponentRelay', function() {
   before(function() {
     mock('GroupConfig',  require('./_group_config').config);
     mock('PersonConfig', require('./_person_config').config);
+    mock('Promise', require('bluebird'));
+    mock('expect', require('chai').expect);
   });
 
   context('connections', {
@@ -33,9 +35,9 @@ objective('ComponentRelay', function() {
 
     });
 
-    before(function(done, bluebird, happner, GroupConfig, PersonConfig) {
+    before(function(done, Promise, happner, GroupConfig, PersonConfig, ComponentRelay) {
       this.timeout(2000);
-      bluebird.all([
+      Promise.all([
         happner.create(GroupConfig(1)),
         happner.create(PersonConfig(1, 1)),
       ])
@@ -45,15 +47,15 @@ objective('ComponentRelay', function() {
       })
       .then(done)./*catch(done)*/catch(function(e) {
         if (! e instanceof Error) {
-          console.log('ERROR', e); // <----------------------------------- TODO: getting errors that are not instanceof Error out of happn/happner, eradicate this behaviour.
+          console.log('ERROR1', e); // <----------------------------------- TODO: getting errors that are not instanceof Error out of happn/happner, eradicate this behaviour.
           return done(new Error());
         }
         done(e);
       });
     });
 
-    after(function(done, bluebird, group1, person1) {
-      bluebird.all([
+    after(function(done, Promise, group1, person1) {
+      Promise.all([
         group1.stop(),
         person1.stop()
       ]).then(done).catch(done);
@@ -62,32 +64,70 @@ objective('ComponentRelay', function() {
 
     it('creates only one connection for multiple relays to the same remote',
 
-      function(done, group1, person1) {
+      function(done, group1, person1, expect, ComponentRelay, happner) {
 
-        // person initiates relay to his/her first private thing
+        // var relayInstance;
+        // mock(ComponentRelay.prototype).spy(
+        //   function create($happn) { // TODO: objective_dev: can't mock on function using injection
+        //     relayInstance = this;
+        //   }
+        // )
 
-        var $happn = person1._mesh.elements.thing1.component.instance; // as injected
+        var relayInstance = group1._mesh.elements.relay.module.instance;
 
-        var relaySpec = {
-          target: $happn.info.datalayer.address,
+        // Ensure one call to MeshClient
+
+        happner.does(
+          function MeshClient() {
+            // console.log('MeshClient()\n', arguments);
+            return mock.original.apply(this, arguments);
+          }
+        );
+
+        var $happn1 = person1._mesh.elements.thing1.component.instance; // as injected
+        var $happn2 = person1._mesh.elements.thing2.component.instance;
+
+        var relaySpec1 = {
+          target: $happn1.info.datalayer.address,
           component: {
             name: 'person1_thing1',
-            description: $happn.description,
+            description: $happn1.description,
           }
         }
 
-        person1.exchange.group1.relay.create(relaySpec)
+        var relaySpec2 = {
+          target: $happn2.info.datalayer.address,
+          component: {
+            name: 'person1_thing2',
+            description: $happn2.description,
+          }
+        }
+
+        // person initiates relay of his/her private thing
+
+        person1.exchange.group1.relay.create(relaySpec1)
 
         .then(function() {
+          expect(  Object.keys( relayInstance.connections )  ).to.eql(['127.0.0.1:20001']);
 
+          // mark the connection
+          relayInstance.connections['127.0.0.1:20001'].mark = 1;
+
+          // relay a second component
+          return person1.exchange.group1.relay.create(relaySpec2)
         })
 
-        .then(done).catch(done);
+        .then(function() {
+          // check for mark (not a new connection)
+          expect(  relayInstance.connections['127.0.0.1:20001'].mark  ).to.equal(1);
+        })
+
+        .then(done).catch(function(e) {
+          console.log('ERROR2', e);
+          done(e);
+        });
       }
     )
-
-    
-
   });
 
 
