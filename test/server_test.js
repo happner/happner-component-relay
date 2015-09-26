f35ca3b2c296853455f05469fd363491304dce51
nomilous <nomilous@gmail.com>
2015-09-26 14:55:07 +0200

    creates relay functions according to description.methods

--- test/server_test.js
+++ test/server_test.js
@@ -3,16 +3,95 @@ objective.only('Server', function() {
   before(function() {
     // mock('GroupConfig',  require('./_group_config').config);
     // mock('PersonConfig', require('./_person_config').config);
-    // mock('Promise', require('bluebird'));
+    mock('Promise', require('bluebird'));
     mock('expect', require('chai').expect);
     mock('should', new require('chai').should());
+  });
+
+  before(function() {
+
+    mock('$happn', {
+      _mesh: {}
+    });
+
+    mock('connection', {
+      exchange: {
+        'target_component': {} // The module at the 'remote' as visible over the connection.
+      }
+    });
+
+    mock('relaySpec', {
+      component: {
+        name: 'relaying_component',
+        description: {
+
+          name: 'target_component',
+          
+          'methods': {
+            exchangeMethod1: {
+              // parameters: []   - NOT SUPPORTING other than standard (arg1, ..., argN, callback)
+              // callback: {
+              //   parameters: [] - NOT SUPPORTING other than standard (error, results)
+              // }
+            },
+            exchangeMethod2: {},
+            exchangeMethod3: {},
+          },
+
+          'routes': {
+            '/thing1/method1': {},
+            '/thing1/method2': {},
+          }
+
+        }
+      }
+    });
+
   });
 
   context('createDynamicModule()', function() {
 
     context('Using Connection', function() {
 
-      it('');
+      it('creates relay functions according to description.methods',
+
+        function(done, Server, expect, $happn, connection, relaySpec, Promise) {
+
+          // Create expectations on the exchange for the target module's methods 
+
+          mock(connection.exchange.target_component).does(
+            function exchangeMethod1(arg1, arg2, callback) {
+              callback(null, 'RESULT ' + arg1 + ' ' + arg2);
+            },
+            function exchangeMethod2(arg1, callback) {
+              callback(null, 'RESULT ' + arg1);
+            },
+            function exchangeMethod3(callback) {
+              callback(null, 'RESULT');
+            }
+          )
+
+          Server.createDynamicModule('connected', $happn, connection, relaySpec, function(e, instance) {
+            if (e) return done(e);
+
+            // Call the instance which relays, should lead to the above expectations being 'met'
+
+            Promise.promisifyAll(instance);
+
+            Promise.all([
+              instance.exchangeMethod1Async('ARG1', 'ARG2'),
+              instance.exchangeMethod2Async('ARG1'),
+              instance.exchangeMethod3Async(),
+            ])
+
+            .then(function(results) {
+              expect(results).to.eql([ 'RESULT ARG1 ARG2', 'RESULT ARG1', 'RESULT' ]);
+            })
+
+            .then(done).catch(done);
+          });
+        }
+      );
       
     });
 
@@ -25,43 +104,6 @@ objective.only('Server', function() {
   });
 
   context('relayConnected()', function() {
-
-    before(function() {
-
-      mock('$happn', {
-        _mesh: {}
-      });
-
-      mock('connection', {
-
-      });
-
-      mock('relaySpec', {
-        component: {
-          name: 'relay_component_name',
-          description: {
-            
-            'methods': {
-              exchangeMethod1: {
-                // parameters: []   - NOT SUPPORTING other than standard (arg1, ..., argN, callback)
-                // callback: {
-                //   parameters: [] - NOT SUPPORTING other than standard (error, results)
-                // }
-              },
-              exchangeMethod2: {
-              },
-            },
-
-            'routes': {
-              '/thing1/method1': {},
-              '/thing1/method2': {},
-            }
-
-          }
-        }
-      });
-
-    });
 
     context('exchange methods', function() {
 
@@ -77,12 +119,12 @@ objective.only('Server', function() {
 
               writeSchema.should.equal(true);
 
-              spec.component.name.should.equal('relay_component_name');
+              spec.component.name.should.equal('relaying_component');
               spec.component.config.should.eql({
-                module: 'relay_component_name'
+                module: 'relaying_component'
               });
 
-              spec.module.name.should.equal('relay_component_name');
+              spec.module.name.should.equal('relaying_component');
 
               // The 'representative' module
               // - dynamically created per relaySpec.component.description
